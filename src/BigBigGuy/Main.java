d94ca31293331d428c824707e680e278c55b5dfc
bsdshell <monosurface@gmail.com>
2017-02-23 14:57:40 -0800

    add list

--- src/BigBigGuy/Main.java
+++ src/BigBigGuy/Main.java
@@ -1,8 +1,14 @@
 package BigBigGuy;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Main {
 
     public static void main(String[] args) {
 	// write your code here
+        List<String> list = new ArrayList<>();
     }
+
+
 }
